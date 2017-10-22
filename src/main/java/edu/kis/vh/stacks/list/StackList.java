80a692dc4bab63b78e894149bca812b7e20b754b
piotr1214 <piotr1214@gmail.com>
2017-10-22 16:44:07 +0200

    3.1.8 Hermetyzacja

--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -4,15 +4,15 @@ public class StackList {
 
 	private static final int EMPTY_STACK = -1;
 	private Node last;
-	public int i;
+	private int i;
 
 	public void pushElement(int i) {
 		if (last == null)
 			last = new Node(i);
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
 		}
 	}
 
@@ -27,15 +27,23 @@ public class StackList {
 	public int peek() {
 		if (empty())
 			return EMPTY_STACK;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (empty())
 			return EMPTY_STACK;
-		int ret = last.value;
-		last = last.prev;
+		int ret = last.getValue();
+		last = last.getPrev();
 		return ret;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
 	}
 
 }
