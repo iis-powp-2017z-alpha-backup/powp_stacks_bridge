7853cd1f31858f86bcefb282f2c77166482cd3f3
Kamil Loos <198161@edu.p.lodz.pl>
2017-10-22 16:53:04 +0200

    3.1.4

---
+++ src/main/java/edu/kis/vh/stacks/factory/defaultStacksFactory.java
@@ -0,0 +1,30 @@
+package edu.kis.vh.stacks.factory;
+
+import edu.kis.vh.stacks.factory.IstacksFactory;
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
+
+public class defaultStacksFactory implements IstacksFactory {
+
+	@Override
+	public Stack getStandardStack() {
+		return new Stack();
+	}
+
+	@Override
+	public Stack getFalseStack() {
+		return new Stack();
+	}
+
+	@Override
+	public StackFIFO getFIFOStack() {
+		return new StackFIFO();
+	}
+
+	@Override
+	public StackHanoi getHanoiStack() {
+		return new StackHanoi();
+	}
+
+}
