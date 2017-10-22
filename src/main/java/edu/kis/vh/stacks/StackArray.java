6abb095d552a5dd2fbaaad1b20996af3f7b08adb
Piotrek <piotrek120017@gmail.com>
2017-10-22 16:54:50 +0200

    [22.10.2017] punkt 4

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,34 +1,40 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.list.StackList;
+
 public class StackArray {
 
-    private int[] ITEMS = new int[12];
+    //private int[] ITEMS = new int[12];
+    private StackList stackList = new StackList();
 
     public int total = -1;
 
     public void push(int i) {
-        if (!isFull())
-            ITEMS[++total] = i;
+//        if (!isFull())
+//            ITEMS[++total] = i;
+    stackList.pushElement(i);
     }
 
     public boolean isEmpty() {
-        return total == -1;
+        return stackList.empty();
     }
 
     public boolean isFull() {
-        return total == 11;
+        return stackList.full();
     }
 
     public int top() {
-        if (isEmpty())
-            return -1;
-        return ITEMS[total];
+//        if (isEmpty())
+//            return -1;
+//        return ITEMS[total];
+        return stackList.peek();
     }
 
     public int pop() {
-        if (isEmpty())
-            return -1;
-        return ITEMS[total--];
+//        if (isEmpty())
+//            return -1;
+//        return ITEMS[total--];
+        return stackList.pop();
     }
 
 }
