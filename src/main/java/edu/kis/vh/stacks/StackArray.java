af85baecd4bb58142262f25fc9fa5ab570452567
unknown <student@L405-5.carbon.cti.p.lodz.pl>
2017-10-22 16:02:37 +0200

    Zadanie 4 Poprawka

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,34 +1,37 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.list.StackList;
+
 public class StackArray {
 
-	private int[] ITEMS = new int[12];
+	//private int[] ITEMS = new int[12];
+	private StackList mArray = new StackList(); 
 
-	public int total = -1;
+	//public int total = -1;
 
 	public void push(int i) {
 		if (!isFull())
-			ITEMS[++total] = i;
+			mArray.pushElement(i);
 	}
 
 	public boolean isEmpty() {
-		return total == -1;
+		return mArray.empty();
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return mArray.full();
 	}
 
 	public int top() {
 		if (isEmpty())
 			return -1;
-		return ITEMS[total];
+		return mArray.peek();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return -1;
-		return ITEMS[total--];
+		return mArray.pop();
 	}
 
 }
--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,32 +1,21 @@
 package edu.kis.vh.stacks;
 
-import edu.kis.vh.stacks.list.StackList;
-
 public class StackFIFO extends stack {
 
-	private StackList mLocalStackList;
-
-	public StackFIFO() {
-		mLocalStackList = new StackList();
-	}
-
-	public StackFIFO(StackList mLocalStackList) {
-		super();
-		this.mLocalStackList = mLocalStackList;
-	}
-
+	public stack temp = new stack();
+	
 	@Override
 	public int pop() {
 		while (!isEmpty())
-
-			mLocalStackList.pushElement(super.pop());
-
-		int ret = mLocalStackList.pop();
-
-		while (!mLocalStackList.empty())
-
-			push(mLocalStackList.pop());
-
+		
+		temp.push(super.pop());
+		
+		int ret = temp.pop();
+		
+		while (!temp.isEmpty())
+	
+		push(temp.pop());
+		
 		return ret;
 	}
 }
