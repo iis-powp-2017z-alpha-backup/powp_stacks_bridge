2cff809e6f5b611d659113d818bc04dd0985c154
student <student@10.0.75.1>
2017-10-22 15:20:52 +0200

    2commit

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -2,33 +2,31 @@ package edu.kis.vh.stacks;
 
 public class stack {
 
-	private int[] ITEMS = new int[12];	
+	private StackArray stackArray;
 
-	public int total = -1;
-	
+	public stack(StackArray stackArray) {
+		super();
+		this.stackArray = stackArray;
+	}
+
 	public void push(int i) {
-		if (!isFull())
-		ITEMS[++total] = i;
+		stackArray.push(i);
+	}
+
+	public boolean isEmpty() {
+		return stackArray.isEmpty();
+	}
+
+	public boolean isFull() {
+		return stackArray.isFull();
+	}
+
+	public int top() {
+		return stackArray.top();
+	}
+
+	public int pop() {
+		return stackArray.pop();
 	}
 	
-		public boolean isEmpty() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-			
-				public int top() {
-					if (isEmpty())
-						return -1;
-					return ITEMS[total];
-				}
-				
-					public int pop() {
-						if (isEmpty())
-							return -1;
-						return ITEMS[total--];
-					}
-				
 }
