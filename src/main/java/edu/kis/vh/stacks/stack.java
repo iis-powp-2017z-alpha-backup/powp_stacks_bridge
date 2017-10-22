56d994f37063271360d72417c47b33494204a4f6
unknown <student@L405-5.carbon.cti.p.lodz.pl>
2017-10-22 15:29:21 +0200

    Punkt 1

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -4,10 +4,35 @@ public class stack {
 
 	private StackArray mLocalStack;
 
+	public stack() {
+		mLocalStack = new StackArray();
+	}
+
 	public stack(StackArray mLocalStack) {
 		super();
 		this.mLocalStack = mLocalStack;
 	}
+
+	public void push(int i) {
+		mLocalStack.push(i);
+	}
+
+	public boolean isEmpty() {
+		return mLocalStack.isEmpty();
+	}
+
+	public boolean isFull() {
+		return mLocalStack.isFull();
+	}
+
+	public int top() {
+		return mLocalStack.top();
+	}
+
+	public int pop() {
+		return mLocalStack.pop();
+	}
+	
 	
 				
 }
