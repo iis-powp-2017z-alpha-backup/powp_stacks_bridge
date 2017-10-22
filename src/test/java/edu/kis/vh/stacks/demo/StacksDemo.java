cf405a64e08c83adcaca0350e2a66c8c27556be1
191874 <191874@CARBON.CTI.P.LODZ.PL>
2017-10-22 15:47:49 +0200

    powp-lab-kod 3.1.1 - poprawa formatowania

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -8,27 +8,33 @@ class StacksDemo {
 
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
-		
-		stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(),
-				factory.GetFIFOStack(), factory.GetHanoiStack()};
-		
+
+		stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(), factory.GetFIFOStack(),
+				factory.GetHanoiStack() };
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				stacks[j].push(i);
-			
-			java.util.Random rn = new java.util.Random();
-			for (int i = 1; i < 15; i++)
-				stacks[3].push(rn.nextInt(20));
-			
-			for (int i = 0; i < stacks.length; i++) {
-				while (!stacks[i].isEmpty())
-					System.out.print(stacks[i].pop() + "  ");
-					System.out.println();
-			}
-		
-		System.out.println("total rejected is "
-				+ ((StackHanoi) stacks[3]).reportRejected());
-		
+
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++)
+			stacks[3].push(rn.nextInt(20));
+
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty())
+				System.out.print(stacks[i].pop() + "  ");
+			System.out.println();
+		}
+
+		System.out.println("total rejected is " + ((StackHanoi) stacks[3]).reportRejected());
+
 	}
-	
+
 }
+
+// StackDecom: linie 19, 20, 21, 23, 24, 25, 26, 27, 29
+// stackTest: linia 15
+// StackHanoi: linie 5, 8, 12, 13
+// StackFIFO: linie 11, 17
+// stack: linie 11-37
+// IstacksFactory: linie 7-17
