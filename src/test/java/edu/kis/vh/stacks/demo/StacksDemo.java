de8a1f3d10f1449c65fdbf6d59c5826962c47d0c
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2017-10-28 17:23:26 +0200

    3.1.1 W komentarzu ilosc zle sformatowanych wierszy

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -16,15 +16,15 @@ class StacksDemo {
 			for (int j = 0; j < 3; j++)
 				stacks[j].push(i);
 			
-			java.util.Random rn = new java.util.Random();
-			for (int i = 1; i < 15; i++)
-				stacks[3].push(rn.nextInt(20));
-			
-			for (int i = 0; i < stacks.length; i++) {
-				while (!stacks[i].isEmpty())
-					System.out.print(stacks[i].pop() + "  ");
-					System.out.println();
-			}
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++)
+			stacks[3].push(rn.nextInt(20));
+		
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty())
+				System.out.print(stacks[i].pop() + "  ");
+			System.out.println();
+		}// 8 wierszy zle formatowanie
 		
 		System.out.println("total rejected is "
 				+ ((StackHanoi) stacks[3]).reportRejected());
