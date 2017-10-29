459a3afc5547e9007464d445b85e0eb8a5be9726
191838 <191838@CARBON.CTI.P.LODZ.PL>
2017-10-29 16:17:54 +0200

    3.3.2
    Generated comments

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,9 +1,19 @@
 package edu.kis.vh.stacks;
 
+/**
+ * @author 191838
+ *
+ */
 public class StackFIFO extends Stack {
 
+	/**
+	 * Temporary stack
+	 */
 	private final Stack temp = new Stack();
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.Stack#pop()
+	 */
 	@Override
 	public int pop() {
 		while (!isEmpty())
