116cfa4cf5ebeb34a3a80658648d52c2c5506432
unknown <bella.aleksander@gmail.com>
2017-11-05 14:12:40 +0100

    Bridge: 1. Przeniesienie klasy StackArray do pakietu, w ktorym znajduje sie StackList 2. Zmiana nazwy tego pakietu

--- src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import edu.kis.vh.stacks.Stack;
-import edu.kis.vh.stacks.StackArray;
+import edu.kis.vh.stacks.impl.StackArray;
 
 public class StackTest {
 
