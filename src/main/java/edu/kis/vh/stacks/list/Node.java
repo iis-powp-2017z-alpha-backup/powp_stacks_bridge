ade15de9a2b3c713ce18c9ea3e980e0289a30172
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2017-11-11 12:06:32 +0100

    3.1.8 Hermetyzacja

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,36 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-	public int value;
-	public Node prev, next;
+	private int value;	//w klasie StackList zmiana na last.getValue()
+	private Node prev;	//w klasie StackList zmiana na last.setPrev oraz last.getPrev
+	private Node next;	//w klasie StackList zmiana na last.setNext oraz last.getNext
 
 	public Node(int i) {
 		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
 	}
 	
 }
