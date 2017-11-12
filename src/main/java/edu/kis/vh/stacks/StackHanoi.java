0914ad5af6e4e820f152611c0e8bde4b7da41dfa
Chrumek <Chrumek@users.noreply.github.com>
2017-11-12 21:40:26 +0100

    3.3.2 Szkielet dokumentacji na przykladzie klasy StackHanoi

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.stacks;
 
+/**
+ * @author Aleksandra Wentlandt 198154
+ *
+ */
 public class StackHanoi extends Stack {
 
 	int totalRejected = 0;
-
+	
+	/**
+	 * @returns the number of integers that where higher than the one on top of the (Hanoi)stack and saves it in variable "totalRejected"
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.Stack#push(int)
+	 * 
+	 */
 	@Override
 	public void push(int in) {
 		if (!isEmpty() && in > top())
