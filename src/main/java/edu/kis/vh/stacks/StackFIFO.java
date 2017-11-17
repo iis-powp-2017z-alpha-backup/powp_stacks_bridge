9d7c636b68db4b6ecd8740e6b3f193195e5f6d76
198161 <198161@edu.p.lodz.pl>
2017-11-17 14:38:13 +0100

    Bridge 3.1.12

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,21 +1,31 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.types.StackList;
+
 public class StackFIFO extends Stack {
 
-	private final Stack temp = new Stack();
+	public StackFIFO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public StackFIFO(StackList stackList) {
+		super(stackList);
+		// TODO Auto-generated constructor stub
+	}
+
+	private final Stack temp = new Stack();
+	
 	@Override
 	public int pop() {
 		while (!isEmpty())
-
 			temp.push(super.pop());
-
-		int ret = temp.pop();
-
+		
+		int poppedOutValue = temp.pop();
+		
 		while (!temp.isEmpty())
-
 			push(temp.pop());
-
-		return ret;
+		
+		return poppedOutValue;
 	}
 }
--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,18 +1,30 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.types.StackList;
+
 public class StackHanoi extends Stack {
 
-	int totalRejected = 0;
+public StackHanoi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackHanoi(StackList stackList) {
+		super(stackList);
+		// TODO Auto-generated constructor stub
+	}
+
+private int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	@Override
-	public void push(int in) {
-		if (!isEmpty() && in > top())
+	public void push(int valueToPush) {
+		if (!isEmpty() && valueToPush > top())
 			totalRejected++;
 		else
-			super.push(in);
+			super.push(valueToPush);
 	}
 }
