72c25b587b3dcb49faf03954dd2e9f4f372b8f40
198161 <198161@edu.p.lodz.pl>
2017-11-17 16:05:25 +0100

    Bridge 3.1.16

--- src/main/java/edu/kis/vh/stacks/factory/IstacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/IstacksFactory.java
@@ -4,14 +4,13 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
 
-public interface IstacksFactory {
-
-	public Stack getStandardStack();
-
-	public Stack getFalseStack();
-
-	public StackFIFO getFIFOStack();
-
-	public StackHanoi getHanoiStack();
-
+public interface IStacksFactory {
+	
+	Stack getStandardStack();
+	
+	Stack getFalseStack();
+	
+	StackFIFO getFIFOStack();
+	
+	StackHanoi getHanoiStack();
 }
--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -3,24 +3,41 @@ package edu.kis.vh.stacks.demo;
 import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+import edu.kis.vh.stacks.factory.StackArrayFactory;
+import edu.kis.vh.stacks.factory.StackListFactory;
+import edu.kis.vh.stacks.factory.IStacksFactory;
 
 class StacksDemo {
 
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
+		StackArrayFactory sAFactory = new StackArrayFactory();
+		StackListFactory sLFactory = new StackListFactory();
 
 		testStacks(factory);
-
+		testStacks(sAFactory);
+		testStacks(sLFactory);
 	}
 
-	private static void testStacks(DefaultStacksFactory factory) {
+	private static void testStacks(IStacksFactory factory) {
 		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(), factory.getFIFOStack(),
 				factory.getHanoiStack() };
 
+		for (int i = 1; i < 15; i++)
+			for (int j = 0; j < 3; j++)
+				stacks[j].push(i);
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++)
+			stacks[3].push(rn.nextInt(20));
+
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty())
+				System.out.print(stacks[i].pop() + "  ");
+			System.out.println();
+		}
+
 		System.out.println("total rejected is " + ((StackHanoi) stacks[3]).reportRejected());
-
 	}
-
 }
 
 // W pliku StacksDemo zmianie ulegly linie 26 - 29
