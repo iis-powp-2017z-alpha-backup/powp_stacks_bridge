4e5c8ab5a8d564a8596a76e2e6f3aaa94bc521f0
Chrumek <zaubererin@poczta.onet.pl>
2017-11-18 22:09:32 +0100

    Bridge 3.1.16

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -2,7 +2,11 @@ package edu.kis.vh.stacks.demo;
 
 import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.factory.ArrayStackFactory;
 import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+import edu.kis.vh.stacks.factory.IstacksFactory;
+import edu.kis.vh.stacks.factory.ListStackFactory;
+
 
 class StacksDemo {
 
@@ -12,13 +16,22 @@ class StacksDemo {
 	private static final int MIN_NUMBER = 1;
 
 	public static void main(String[] args) {
-		DefaultStacksFactory factory = new DefaultStacksFactory();
-
-		testStacks(factory);
+		DefaultStacksFactory defaultStack = new DefaultStacksFactory();
+		ListStackFactory listStackfactory = new ListStackFactory();
+		ArrayStackFactory arrayStackfactory = new ArrayStackFactory();
+		
+		System.out.println("****DefaultStacksFactory****");
+		testStacks(defaultStack);
+		System.out.println("\n");
+		System.out.println("****ListStackFactory****");
+		testStacks(listStackfactory);
+		System.out.println("\n");
+		System.out.println("****ArrayStackFactory****");
+		testStacks(arrayStackfactory);
 
 	}
 
-	private static void testStacks(DefaultStacksFactory factory) {
+	private static void testStacks(IstacksFactory factory) {
 		//TODO: there is enough place for "factory.getHanoiStack()" in the upper line. So it shoudn't be moved to the lower line 
 		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(), factory.getFifoStack(),
 				factory.getHanoiStack() };
