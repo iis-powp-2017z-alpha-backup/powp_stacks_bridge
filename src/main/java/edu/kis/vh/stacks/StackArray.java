206b568129962c6240e40e6c6f63d8b156f1ce27
Chrumek <zaubererin@poczta.onet.pl>
2017-11-18 11:48:55 +0100

    Bridge 3.1.1. klasa StackArray

---
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -0,0 +1,41 @@
+package edu.kis.vh.stacks;
+
+public class StackArray {
+	private static final int NUMB_NNEXT = 11;
+
+	private static final int TABLE_SIZE = 12;
+
+	private final int[] ITEMS = new int[TABLE_SIZE];
+
+	private int total = -1;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void push(int i) {
+		if (!isFull())
+			ITEMS[++total] = i;
+	}
+
+	public boolean isEmpty() {
+		return total == -1;
+	}
+
+	public boolean isFull() {
+		return total == NUMB_NNEXT;
+	}
+
+	public int top() {
+		if (isEmpty())
+			return -1;
+		return ITEMS[total];
+	}
+
+	public int pop() {
+		if (isEmpty())
+			return -1;
+		return ITEMS[total--];
+	}
+
+}
