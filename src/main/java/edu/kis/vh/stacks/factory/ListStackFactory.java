8303c5ec6d871c5cd229ba101db2ee7d8e3f7f40
Chrumek <zaubererin@poczta.onet.pl>
2017-11-18 21:46:12 +0100

    Bridge 3.1.15

---
+++ src/main/java/edu/kis/vh/stacks/factory/ListStackFactory.java
@@ -0,0 +1,35 @@
+package edu.kis.vh.stacks.factory;
+
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFifo;
+import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.stacksinterfaces.StackArray;
+import edu.kis.vh.stacks.stacksinterfaces.StackList;
+
+public class ListStackFactory implements IstacksFactory {
+
+	@Override
+	public Stack getStandardStack() {
+		// TODO Auto-generated method stub
+		return new Stack(new StackList());
+	}
+
+	@Override
+	public Stack getFalseStack() {
+		// TODO Auto-generated method stub
+		return new Stack(new StackArray());
+	}
+
+	@Override
+	public StackFifo getFifoStack() {
+		// TODO Auto-generated method stub
+		return new StackFifo();
+	}
+
+	@Override
+	public StackHanoi getHanoiStack() {
+		// TODO Auto-generated method stub
+		return new StackHanoi();
+	}
+
+}
