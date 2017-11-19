9a470a524cab6687f1d983fa32353925dd0a1e49
198176 <adriantomczak36@gmail.com>
2017-11-19 15:17:19 +0100

    Przygotowanie fabryk

---
+++ src/main/java/edu/kis/vh/stacks/factory/StackFactory.java
@@ -0,0 +1,11 @@
+package edu.kis.vh.stacks.factory;
+
+import edu.kis.vh.stacks.Stack;
+
+public interface StackFactory {
+
+	public Stack getStandardStack();
+
+	public Stack getFalseStack();
+	
+}
