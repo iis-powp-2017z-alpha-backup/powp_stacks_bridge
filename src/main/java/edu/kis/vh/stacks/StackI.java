da6db4e259d47ead4eb699681ebf3a56fa3d1df0
TotallyUniqueUsername <andrzejstrek@o2.pl>
2017-11-28 23:02:10 +0100

    dzialajace do pkt9 - update

---
+++ src/main/java/edu/kis/vh/stacks/StackI.java
@@ -0,0 +1,15 @@
+package edu.kis.vh.stacks;
+
+public interface StackI {
+
+	void push(int i);
+
+	boolean isEmpty();
+
+	boolean isFull();
+
+	int pop();
+
+	int top();
+
+}
---
+++ src/main/java/edu/kis/vh/stacks/stackIImpl/StackArray.java
@@ -0,0 +1,36 @@
+package edu.kis.vh.stacks.stackIImpl;
+
+import edu.kis.vh.stacks.StackI;
+
+public class StackArray implements StackI{
+
+	private int[] ITEMS = new int[12];
+
+	public int total = -1;
+
+	public void push(int i) {
+		if (!isFull())
+			ITEMS[++total] = i;
+	}
+
+	public boolean isEmpty() {
+		return total == -1;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	public int top() {
+		if (isEmpty())
+			return -1;
+		return ITEMS[total];
+	}
+
+	public int pop() {
+		if (isEmpty())
+			return -1;
+		return ITEMS[total--];
+	}
+
+}
