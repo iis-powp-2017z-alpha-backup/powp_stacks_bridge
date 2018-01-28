1f83064ad8b295244d0a302b2a01d49871fac9e5
tbodzak <tomaszbodzak@gmail.com>
2018-01-28 15:43:26 +0100

    commit do punktu 3.1.14

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -14,7 +14,7 @@ public class StackFIFO extends Stack {
 		// TODO Auto-generated constructor stub
 	}
 
-	private final Stack temp = new Stack();
+	private final Stack temp = new Stack(new StackList());
 	
 	@Override
 	public int pop() {
@@ -29,3 +29,5 @@ public class StackFIFO extends Stack {
 		return poppedOutValue;
 	}
 }
+
+//Najlepszym wyborem bedzie StackList z uwagi na mozliwosc usuwania elementow
