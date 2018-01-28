f29c38817d2ece58febe842cdca1ae26ddfb7be4
tbodzak <tomaszbodzak@gmail.com>
2018-01-28 15:35:01 +0100

    commit do punktu 3.1.11

--- src/main/java/edu/kis/vh/stacks/IStack.java
+++ src/main/java/edu/kis/vh/stacks/IStack.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.stacks;
 
 public interface IStack {
-
-	final int EMPTY_STACK_VALUE = -1;
+	
+	final int EMPTY_STACK_VALUE = 0;
 
 	void push(int i);
 
@@ -15,3 +15,5 @@ public interface IStack {
 	int pop();
 
 }
+
+//Realizacja zadania 10 pomogla, poniewaz wystarczyło zmienic wartosc we wspolnym interface.
