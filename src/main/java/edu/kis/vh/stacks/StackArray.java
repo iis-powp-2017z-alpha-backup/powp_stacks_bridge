bf251f6325a93a6d6e990f45b329b79585f891f4
tbodzak <tomaszbodzak@gmail.com>
2018-01-28 15:02:57 +0100

    commit do punktu 3.1.5

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.stacks;
 
-public class StackArray {
+public class StackArray implements IStack {
 	final int EMPTY_STACK_VALUE = -1;
 	final int FULL_STACK_VALUE = 11;
 
@@ -12,25 +12,30 @@ public class StackArray {
 		return total;
 	}
 
+	@Override
 	public void push(int i) {
 		if (!isFull())
 		ITEMS[++total] = i;
 	}
 	
+	@Override
 	public boolean isEmpty() {
 		return total == EMPTY_STACK_VALUE;
 	}
 	
+	@Override
 	public boolean isFull() {
 		return total == FULL_STACK_VALUE;
 	}
 		
+	@Override
 	public int top() {
 		if (isEmpty())
 			return EMPTY_STACK_VALUE;
 		return ITEMS[total];
 	}
 			
+	@Override
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_STACK_VALUE;
