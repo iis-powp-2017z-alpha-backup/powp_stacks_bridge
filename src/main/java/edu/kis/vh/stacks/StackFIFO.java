de54a4589eb29dbb3b5b664774b903947cd38096
Więcek.Mateusz <mateurzx@gmail.com>
2018-01-31 23:58:49 +0100

    3.1.12 -.14

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,8 +1,16 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.imps.StackArray;
+
 public class StackFIFO extends Stack {
 
-    public Stack temp = new Stack();
+    public StackArray temp = new StackArray();
+    // lepsze odwolanie do hierarchi bo skraca droge
+
+    public StackFIFO(StackImp items) {
+        super(items);
+        // TODO Auto-generated constructor stub
+    }
 
     @Override // override
     public int pop() {
