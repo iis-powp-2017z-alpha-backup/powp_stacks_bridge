61b68d1329796c2dd6a99a34fef409a6720fb9f2
Więcek.Mateusz <mateurzx@gmail.com>
2018-01-31 22:53:06 +0100

    Bridge 3.1.1 utworzenie nowej klasy stackarray

---
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -0,0 +1,50 @@
+package edu.kis.vh.stacks;
+
+public class StackArray {
+
+    private static final int _twelve = 12;  // final
+
+    private static final int MINUSONE = -1; // final
+
+    private int[] items = new int[_twelve];
+
+    private int total = MINUSONE;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void push(int i) {
+        if (!isFull()) {
+            getItems()[++total] = i;
+        }
+    }
+
+    private int[] getItems() {
+        return items;
+    }
+
+    public boolean isEmpty() {
+        return total == MINUSONE;
+    }
+
+    public boolean isFull() {
+
+        return total == 11;
+    }
+
+    public int top() {
+        if (isEmpty()) {
+            return MINUSONE;
+        }
+        return getItems()[total];
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            return MINUSONE;
+        }
+        return getItems()[total--];
+    }
+
+}
