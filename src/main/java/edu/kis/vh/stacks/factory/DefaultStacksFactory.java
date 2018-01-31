219d8b19122e43a80c9696d1195b19cc88ac6139
Więcek.Mateusz <mateurzx@gmail.com>
2018-01-31 21:49:09 +0100

    Bledy formatowania usuniete 311-313

--- src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
@@ -1,30 +1,29 @@
 package edu.kis.vh.stacks.factory;
 
-import edu.kis.vh.stacks.factory.IstacksFactory;
-import edu.kis.vh.stacks.stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.stack;
 
 public class DefaultStacksFactory implements IstacksFactory {
 
-	@Override
-	public stack GetStandardStack() {
-		return new stack();
-	}
+    @Override
+    public stack GetStandardStack() {
+        return new stack();
+    }
 
-	@Override
-	public stack GetFalseStack() {
-		return new stack();
-	}
+    @Override
+    public stack GetFalseStack() {
+        return new stack();
+    }
 
-	@Override
-	public StackFIFO GetFIFOStack() {
-		return new StackFIFO();
-	}
+    @Override
+    public StackFIFO GetFIFOStack() {
+        return new StackFIFO();
+    }
 
-	@Override
-	public StackHanoi GetHanoiStack() {
-		return new StackHanoi();
-	}
+    @Override
+    public StackHanoi GetHanoiStack() {
+        return new StackHanoi();
+    }
 
 }
