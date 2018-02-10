a0ae522ff40562e4438fbb07e932f41675e1aa64
byczus1990 <byczek.szymon@gmail.com>
2018-02-10 13:56:02 +0100

    Zad od 5 do 7

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,45 +1,48 @@
 package edu.kis.vh.stacks;
 
-import edu.kis.vh.stacks.list.StackList;
+public class StackArray implements StackInterface {
 
-public class StackArray {
 
-    //private int[] ITEMS = new int[12];
-	private StackList stackList = new StackList();
+	private int[] ITEMS = new int[12];
+    
+	public int total = -1;
 	
-    public int total = -1;
-
-    public void push(int i) {
-        //if (!isFull())
-            //ITEMS[++total] = i;
-    	stackList.pushElement(i);
+	@Override
+	public void push(int i) {
+        if (!isFull())
+            ITEMS[++total] = i;
+    	//stackList.pushElement(i);
     }
-
+	
+	@Override
     public boolean isEmpty() {
-       // return total == -1;
-    	return stackList.empty();
+        return total == -1;
+    	//return stackList.empty();
     }
 
+	@Override
     public boolean isFull() {
-        //return total == 11;
-    	return stackList.full();
+        return total == 11;
+    	//return stackList.full();
     	
     }
 
+	@Override
     public int top() {
-        /*if (isEmpty())
+        if (isEmpty())
             return -1;
         return ITEMS[total];
-    	*/
-    	return stackList.peek();
+    	
+    	//return stackList.peek();
     }
 
+	@Override 
     public int pop() {
-        /*if (isEmpty())
+        if (isEmpty())
             return -1;
         return ITEMS[total--];
-        */
-    	return stackList.pop();
+        
+    	//return stackList.pop();
     }
 
 }
--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -1,38 +1,42 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.list.StackList;
 
 public class stack {
-
-
+	/*
+	Wykonana zmiana pozwala na wykorzystanie jako atrybutu obiektu dowolnej klasy 
+	implementujacej interfejs StackInterface. Daje to mozliwosc przy zmianie 
+	przekazywanego obiektu zmiane wykorzystywanej implementacji stosu.
+	*/
 	
-	private StackArray stackArray;
+	private StackInterface stackInterface;
 	
 		public stack () {
-			this.stackArray = new StackArray();
+			this.stackInterface = new StackList();
 		}
 	
-		public stack(StackArray stackArray) {
-			this.stackArray = stackArray;
+		public stack(StackInterface stackInterface) {
+			this.stackInterface = stackInterface;
 		}
 
 	
 	public void push(int i) {
-		stackArray.push(i);
+		stackInterface.push(i);
 	}
 
 	public boolean isEmpty() {
-		return stackArray.isEmpty();
+		return stackInterface.isEmpty();
 	}
 
 	public boolean isFull() {
-		return stackArray.isFull();
+		return stackInterface.isFull();
 	}
 
 	public int top() {
-		return stackArray.top();
+		return stackInterface.top();
 	}
 	public int pop() {
-		return stackArray.pop();
+		return stackInterface.pop();
 	}
 
 }
