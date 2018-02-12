bccaf29dcd9dddbc1f15eae5f897bb77389aa230
Damian Stepnik <edamexxx@gmail.com>
2018-02-13 00:56:37 +0100

    Hermetyzacja kilku pól
    [lab1] Punkt 3.1.8

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,37 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-	public int value;
-	public Node prev, next;
+	// ustawienie tych pol jako prywatne i wygenerowanie dla nich setterow i getterow powoduje, ze odowujac sie do tych
+	// wartosci, nalezy skorzystac z metod setX lub getX np. last.getValue() zamiast last.value w klasie StackList
+	private int value;
+	private Node prev, next;
 
 	public Node(int i) {
 		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
 	}
 
 }
