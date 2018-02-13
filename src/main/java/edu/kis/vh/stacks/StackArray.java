bf5f06ac3e8af77a116a908dd318a4c98f2ab12a
Damian Stepnik <edamexxx@gmail.com>
2018-02-13 19:15:55 +0100

    Utworzenie klasy StackArray
    [lab2] Punkt 3.1.1

---
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -0,0 +1,44 @@
+package edu.kis.vh.stacks;
+
+public class StackArray {
+	
+	private static final int STACK_EMPTY = -1;
+
+	private static final int STACK_CAPACITY = 12;
+
+	private static final int STACK_MAX_INDEX = STACK_CAPACITY - 1;
+
+	private final int[] items = new int[STACK_CAPACITY];
+
+	private int total = STACK_EMPTY;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void push(int i) {
+		if (!isFull())
+			items[++total] = i;
+	}
+
+	public boolean isEmpty() {
+		return total == STACK_EMPTY;
+	}
+
+	public boolean isFull() {
+		return total == STACK_MAX_INDEX;
+	}
+
+	public int top() {
+		if (isEmpty())
+			return STACK_EMPTY;
+		return items[total];
+	}
+
+	public int pop() {
+		if (isEmpty())
+			return STACK_EMPTY;
+		return items[total--];
+	}
+
+}
